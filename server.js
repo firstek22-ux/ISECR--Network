1d7f822a5e2bbcfc3bf90cb5653a1889a88f03cd
firstek22-ux <firstek22@gmail.com>
2026-08-05 20:46:37 +0800

    更新 server.js

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./database");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+db.run(`
+
+CREATE TABLE IF NOT EXISTS posts(
+
+id INTEGER PRIMARY KEY,
+
+author TEXT,
+
+content TEXT,
+
+time TEXT,
+
+likes INTEGER,
+
+likedBy TEXT
+
+)
+
+`);
 
 
 // ======================
